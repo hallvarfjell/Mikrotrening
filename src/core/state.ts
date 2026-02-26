491851763378c5b6f7b7f176ee79279bdf07534e
hallvarfjell <hallvarfjell@hotmail.com>
2026-02-26 16:24:08 +0100

    Delete src directory

--- src/core/state.ts
+++
@@ -1,8 +0,0 @@
-export type State = 'IDLE'|'ACTIVE_EXERCISE'|'REST'|'PAUSED'|'STOP'|'COMPLETED';
-
-
-export class StateMachine {
-private state: State = 'IDLE';
-getState() { return this.state; }
-transition(to: State) { this.state = to; }
-}

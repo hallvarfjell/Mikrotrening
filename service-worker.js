5eea30213d8514e629ef61b4d5f21afaa1b20033
hallvarfjell <hallvarfjell@hotmail.com>
2026-03-01 10:41:58 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,6 @@
 
-self.addEventListener('install', e => {
-  console.log('Service worker install');
-});
-self.addEventListener('fetch', e => {});
+const CACHE='intervall-cache-v22';
+const ASSETS=['./','./index.html','./builder.html','./results.html','./log.html','./settings.html','./style.css','./main.js','./builder.js','./results.js','./manifest.json'];
+self.addEventListener('install',e=>{ self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); });
+self.addEventListener('activate',e=>{ e.waitUntil((async()=>{ const keys=await caches.keys(); await Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))); await self.clients.claim(); })()); });
+self.addEventListener('fetch',e=>{ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });

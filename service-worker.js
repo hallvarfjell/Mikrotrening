3979e99783018e884a1799f04488d44882a67ae3
hallvarfjell <hallvarfjell@hotmail.com>
2026-04-06 20:23:20 +0200

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,8 +0,0 @@
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('laps-cache').then(cache => {
-      return cache.addAll(['./','index.html','styles.css','app.js']);
-    })
-  );
-});
